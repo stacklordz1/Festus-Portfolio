fb385ef279e61bc7f0a10ce1faa19dca9d601259
Festus <chidubemoffiaukwu5@gmail.com>
2026-02-09 22:10:25 +0000

    updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Portfolio in Progress
 Check in soon
+Backup
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a placeholder for my portfolio website. Stay tuned for updates!</p>S
     </div>
   )
 }
